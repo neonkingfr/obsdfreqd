b2807f27971bee5571c5b2f878d07cbb27dd674b
Solene Rapenne <solene@perso.pw>
2022-03-16 19:36:06 +0100

    obsdfreqd: add flags and usage

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <err.h>
+#include <math.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <time.h>
@@ -28,16 +29,48 @@ void quit_gracefully() {
     exit(0);
 }
 
-int main() {
+void usage() {
+    printf("obsdfreqd [-l min_freq]  [-m max_freq] [-s step]\n");
+}
 
+int main(int argc, char *argv[]) {
+
+    int opt;
     int mib_perf[2];
     int mib_powerplug[2];
     int mib_load[2];
     long cpu[CPUSTATES], cpu_previous[CPUSTATES];
     int frequency;
-    int value;
+    int value, current_frequency;
     int cpu_usage_total = 0, cpu_usage;
     size_t len, len_cpu;
+
+    int min = 0;
+    int max = 100;
+    int step = 10;
+
+    while((opt = getopt(argc, argv, "l:m:s:")) != -1) {
+        switch(opt) {
+        case 'l':
+            min = atoi(optarg);
+            if(min > 100 || min < 0)
+                err(1, "minimum frequency must be between 0 and 100");
+            break;
+        case 'm':
+            max = atoi(optarg);
+            if(max > 100 || max < 0)
+                err(1, "maximum frequency must be between 0 and 100");
+            break;
+        case 's':
+            step = atoi(optarg);
+            if(step > 100 || step <= 0)
+                err(1, "step must be positive and up to 100");
+            break;
+        default:
+           usage();
+           return 1;
+        }
+    }
 
     mib_perf[0] = CTL_HW;
     mib_perf[1] = HW_SETPERF;
@@ -57,6 +90,7 @@ int main() {
     // avoid weird reading for first delta
     if (sysctl(mib_load, 2, &cpu_previous, &len_cpu, NULL, 0) == -1)
         err(1, "sysctl");
+    usleep(1000*500);
 
     /* main loop */
     for(;;) {
@@ -66,9 +100,9 @@ int main() {
         printf("power: %i |", value);
 
         // get current frequency
-        if (sysctl(mib_perf, 2, &value, &len, NULL, 0) == -1)
+        if (sysctl(mib_perf, 2, &current_frequency, &len, NULL, 0) == -1)
             err(1, "sysctl");
-        printf("perf: %3i |", frequency);
+        printf("perf: %3i |", current_frequency);
 
         // get where the CPU time is spent, last field is IDLE
         if (sysctl(mib_load, 2, &cpu, &len_cpu, NULL, 0) == -1)
@@ -79,25 +113,42 @@ int main() {
             cpu[1]-cpu_previous[1] +
             cpu[2]-cpu_previous[2] +
             cpu[3]-cpu_previous[3] +
-            cpu[4]-cpu_previous[4];
+            cpu[4]-cpu_previous[4] +
+            cpu[5]-cpu_previous[5];
 
-        if(cpu_usage == 0)
-            cpu_usage_total = 0;
-        else
-            cpu_usage_total = 100-((cpu[5]-cpu_previous[5])-cpu_usage);
+        // debug
+        //printf("\nDEBUG: User: %3i\tNice: %3i\t Sys: %3i\tSpin: %3i\t Intr: %3i\tIdle: %3i\n",
+        //       cpu[0]-cpu_previous[0],
+        //       cpu[1]-cpu_previous[1],
+        //       cpu[2]-cpu_previous[2],
+        //       cpu[3]-cpu_previous[3],
+        //       cpu[4]-cpu_previous[4],
+        //       cpu[5]-cpu_previous[5]);
+        //printf("cpu usage = %i et idle = %i\n", cpu_usage, cpu[5] - cpu_previous[5]);
+
+        cpu_usage_total = 100-round(100*(cpu[5]-cpu_previous[5])/cpu_usage);
         memcpy(cpu_previous, cpu, sizeof(cpu));
         printf("usage: %3i%% |", cpu_usage_total);
 
         // change frequency
         len = sizeof(frequency);
+
+        // small brain condition to increase CPU
         if(cpu_usage_total > 30) {
-            if(frequency < 100)
-                frequency = frequency + 10;
+
+            // increase frequency by step if under max
+            if(frequency < max)
+                frequency = frequency + step;
+
+            // don't try to set frequency more than 100%
+            if( frequency > 100 )
+                frequency = 100;
+
             if (sysctl(mib_perf, 2, NULL, 0, &frequency, len) == -1)
                 err(1, "sysctl");
         }
         else {
-            frequency = 0;
+            frequency = min;
             if (sysctl(mib_perf, 2, NULL, 0, &frequency, len) == -1)
                 err(1, "sysctl");
         }
